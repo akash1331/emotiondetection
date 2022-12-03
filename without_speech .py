6cb8b4cd59d506b82f960ac3427cba027f456e20
akash1331 <akashpk1331@gmail.com>
2022-12-03 11:54:23 +0530

    no_speech

---
+++ without_speech .py
@@ -0,0 +1,60 @@
+from keras.models import load_model
+from time import sleep
+import tensorflow
+from keras.utils import img_to_array
+import cv2
+import numpy as np
+import pyttsx3
+
+# --------------------------------- 
+# # text to speech
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+engine.setProperty('rate',150)
+# engine.say(predicted_emotion)
+engine.runAndWait()
+
+def speak(str):
+    engine.say(str)
+    engine.runAndWait()
+# ---------------------------------
+
+
+face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+classifier =load_model("model.h5")
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            # print(label)
+
+            # speak(label)     #text to speech
+
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
